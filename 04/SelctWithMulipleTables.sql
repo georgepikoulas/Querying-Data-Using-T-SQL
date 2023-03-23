6d9da31a0651d88f1ace049548223a6910bde6c2
geopik <geopik@gmail.com>
2023-03-23 17:23:13 +0200

    inner join will retrun only rows from mboth tables that satisfy the condition. We will not get Custoemrs without any orders

--- 04/SelctWithMulipleTables.sql
+++ 04/SelctWithMulipleTables.sql
@@ -1,4 +1,6 @@
 use TSQLDemoDB ;
-select * from Customers 
-cross join  -- will return a column for Custoemrs from both tables , which is not makng sense!
-Orders
+select * from Customers  as c 
+--cross join  -- cross join will return a column for Custoemrs from both tables , which is not makng sense!
+inner join --inner join will retrun only rows from mboth tables that satisfy the condition
+Orders as o
+on c.Customer = o.Customer

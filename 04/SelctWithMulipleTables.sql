92efe5cb37d6717712714ec6d118f8b0221dfb3a
geopik <geopik@gmail.com>
2023-03-23 17:26:13 +0200

    left outer join This way we can het all rows from the Left Table in the join all rowsas well. Meaniong Customers without Orders

--- 04/SelctWithMulipleTables.sql
+++ 04/SelctWithMulipleTables.sql
@@ -1,6 +1,7 @@
 use TSQLDemoDB ;
 select * from Customers  as c 
 --cross join  -- cross join will return a column for Custoemrs from both tables , which is not makng sense!
-inner join --inner join will retrun only rows from mboth tables that satisfy the condition
+--inner join --inner join will retrun only rows from mboth tables that satisfy the condition. We will not get Customers without any orders
+left outer join -- left outer join This way we can het all rows from the Left Table in the join all rowsas well. Meaniong Customers without Orders
 Orders as o
 on c.Customer = o.Customer
